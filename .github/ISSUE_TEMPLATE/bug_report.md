c84b6e58cfd7796b414d821994462b7c1c16a614
Matthieu <matthieu@pixel-developpement.com>
2024-03-24 18:47:13 +0100

    + Add github action
    + Add easy-coding-standard

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,25 @@
+---
+name: "Bug report"
+about: "If you find something strange or a functionnality don't work like expected"
+---
+
+## Describe the bug
+
+## Steps to reproduce
+
+1.
+1.
+1.
+
+## Expected behavior
+
+## Additional context
+
+E.g. screenshots, logs, video
+
+## Context
+
+- Browser:
+- Magento version:
+- Module version:
+- Module config:
---
+++ ecs.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/Block',
+        __DIR__ . '/Controller',
+        __DIR__ . '/CustomerData',
+        __DIR__ . '/Helper',
+        __DIR__ . '/Plugin',
+        __DIR__ . '/Session',
+        __DIR__ . '/view',
+    ]);
+
+    // this way you add a single rule
+    $ecsConfig->rules([
+        NoUnusedImportsFixer::class,
+    ]);
+
+    // this way you can add sets - group of rules
+    $ecsConfig->sets([
+        SetList::SPACES,
+        SetList::ARRAY,
+        SetList::DOCBLOCK,
+        SetList::NAMESPACES,
+        SetList::COMMENTS,
+        SetList::PSR_12,
+        SetList::SYMPLIFY
+    ]);
+};
